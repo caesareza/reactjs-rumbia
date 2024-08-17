ce1cfb317fb602db13181435137332eb6b02dc81
dreas <andreas.caesareza@erajaya.com>
2024-08-17 10:07:24 +0700

    RM-2 delete MenuBar component add gogole font and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { Routes, Route, Outlet } from "react-router-dom";
-
-import MenuBar from './components/MenuBar';
-import SiteFooter from './components/SiteFooter';
-import Home from "./pages/Home";
-import About from "./pages/About";
-import PageNotFound from "./pages/PageNotFound";
+import { Routes, Route, Outlet } from 'react-router-dom'
+import SiteFooter from './components/SiteFooter'
+import Home from './pages/Home'
+import About from './pages/About'
+import PageNotFound from './pages/PageNotFound'
+import SiteHeader from './components/SiteHeader.jsx'
 
 function Layout() {
-    return(
+    return (
         <>
-            <MenuBar />
-            <Outlet />
+            <SiteHeader />
+            <main>
+                <Outlet />
+            </main>
             <SiteFooter />
         </>
     )
 }
 
 function App() {
-  return (
-      <Routes>
-          <Route path="/" element={<Layout />}>
-              <Route index path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="*" element={<PageNotFound />} />
-          </Route>
-      </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Route>
+        </Routes>
+    )
 }
 
-export default App;
-
-
-
+export default App
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,24 +1,23 @@
-import kaguraImage from '/kagura.jpg';
-import Image, { Avatar } from './Common/Image';
-import { numberFormat, abbreviateNumber } from '../utils/number-utils';
+import Image from './Common/Image'
+import { numberFormat, abbreviateNumber } from '../utils/number-utils'
 
 const MainPage = () => (
-  <main>
-    <h1>Explore Bali</h1>
+    <>
+        <h1>Explore Bali</h1>
 
-    <Image
-      src="/photos/2711640/pexels-photo-2711640.jpeg"
-      auto="compress"
-      cs="tinysrgb"
-      width={300}
-    />
+        <Image
+            src="/photos/2711640/pexels-photo-2711640.jpeg"
+            auto="compress"
+            cs="tinysrgb"
+            width={300}
+        />
 
-    <ul>
-      <li>Kuta {abbreviateNumber(10000)}</li>
-      <li>Seminyak {numberFormat(50000)}</li>
-      <li>Denpasar {numberFormat(5000)}</li>
-    </ul>
-  </main>
-);
+        <ul>
+            <li>Kuta {abbreviateNumber(10000)}</li>
+            <li>Seminyak {numberFormat(50000)}</li>
+            <li>Denpasar {numberFormat(5000)}</li>
+        </ul>
+    </>
+)
 
-export default MainPage;
+export default MainPage
