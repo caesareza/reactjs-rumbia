62d07b926732d1c96036f8fe2f24b538a1e0e98d
dreas <andreas.caesareza@erajaya.com>
2024-08-17 10:10:14 +0700

    RM-3 disable react props type validation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 export default function Home() {
-    return(
+    return (
         <section className="home-page">
             <h1>Home Page</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolorum excepturi exercitationem expedita incidunt inventore ipsa laudantium mollitia, neque odio optio, perferendis provident quam sint tenetur. Exercitationem neque numquam tempore. </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid dolorum excepturi exercitationem expedita incidunt
+                inventore ipsa laudantium mollitia, neque odio optio,
+                perferendis provident quam sint tenetur. Exercitationem neque
+                numquam tempore.{' '}
+            </p>
         </section>
     )
 }
